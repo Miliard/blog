17ed77de73fa849b0068e39ebd55fba1126ae184
envy <elsalvadorazul@ouitlook.es>
2025-07-09 16:46:32 -0600

    saliendo del capitulo 18

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,53 @@ Route::get('/', function () {
 
     return "hola desde la pagina de inicio";
 
-    // return view('welcome');
-
     });
 
-    Route::get('/contacto', function(){
 
-        return "hola desde la pagina contactos";
-    });
 
+
+// ruta para mostrar el listado de registro
+Route::get('/posts', function(){
+
+    return "hola desde la pagina de posts";
+
+});
+
+//! ruta para ostrar un formulariopara crear un registro
+Route::get('/posts/create', function(){  //? ruta para mostrar un formulario para crear un registro
+
+    return "aqui de mostrara el formulario para crear posts";
+
+});
+
+
+//@ ruta para guardar un registro
+Route::post('/posts', function(){
+    return "aqui se procesara el formulario para crear el posts";
+
+});
+
+//! ruta para mostrar un registro
+
+Route::get('/posts/{post}', function ($post){
+    return " aqui se motrara el post: $post";
+});
+
+//? ruta para mostrar un formulario para esditar un registro
+
+Route::get('/posts/{post}/edit', function ($post) {
+
+    return " aqui se mostrara el formulario para editar posts: $post";
+});
+
+//* ruta para actualizar un registro
+Route::put('/posts/{post}',function ($post){
+
+    return "aqui se procesara el formulario para editar el post: $post";
+});
+
+
+//todo ruta para eliminar un registro
+Route::delete('.posts/{post}', function ($post){
+    return "aqui se eliminara el post: $post";
+});
