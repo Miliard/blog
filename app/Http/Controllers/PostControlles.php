a621039ecfcb689c5a22d175086d4fd17c2da76d
envy <elsalvadorazul@ouitlook.es>
2025-07-29 17:05:37 -0600

    saliendo del capitulo 20 a mediado

--- app/Http/Controllers/PostControlles.php
+++ app/Http/Controllers/PostControlles.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Pest\Plugins\Retry;
 
 class PostControlles extends Controller
 {
     public function index(){
 
     return "hola desde la pagina de posts y desde el controlador";
+    }
+
+    public function create(){
+        return "aqui se mostrara el formulario para crear post";
+    }
+
+    public function store(){
+        return "aqui se procesara el formulario para crear un post";
+    }
+
+    public function show($post){
+            return " aqui se motrara el post: $post";
+    }
+
+    public function edit($post){
+     return " aqui se mostrara el formulario para editar posts: $post";
+    }
+
+    public function update($post){
+        return "aqui se procesara el formulario para editar el post: $post";
+    }
+
+    public function destroy($post){
+        return "aqui se eliminara el post: $post";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,49 +10,23 @@ Route::get('/', function () {
 
     });
 
-
-
-
 //*  ruta para mostrar el listado de registro
 Route::get('/posts', [PostControlles::class, 'index']);
 
+//* ruta para ostrar un formulariopara crear un registro
+Route::get('/posts/create', [PostControlles::class, 'create']);
 
+//* ruta para guardar un registro
+Route::get('/posts/store', [PostControlles::class, 'store']);
 
-//! ruta para ostrar un formulariopara crear un registro
-Route::get('/posts/create', function(){  //? ruta para mostrar un formulario para crear un registro
+//* ruta para mostrar un registro
+Route::get('/posts/{post}', [PostControlles::class, 'show']);
 
-    return "aqui de mostrara el formulario para crear posts";
-
-});
-
-
-//@ ruta para guardar un registro
-Route::post('/posts', function(){
-    return "aqui se procesara el formulario para crear el posts";
-
-});
-
-//! ruta para mostrar un registro
-
-Route::get('/posts/{post}', function ($post){
-    return " aqui se motrara el post: $post";
-});
-
-//? ruta para mostrar un formulario para esditar un registro
-
-Route::get('/posts/{post}/edit', function ($post) {
-
-    return " aqui se mostrara el formulario para editar posts: $post";
-});
+//* ruta para mostrar un formulario para esditar un registro
+Route::get('/posts/{post}/edit', [PostControlles::class, 'edit']);
 
 //* ruta para actualizar un registro
-Route::put('/posts/{post}',function ($post){
+Route::put('/posts/{post}',[PostControlles::class, 'update']);
 
-    return "aqui se procesara el formulario para editar el post: $post";
-});
-
-
-//todo ruta para eliminar un registro
-Route::delete('.posts/{post}', function ($post){
-    return "aqui se eliminara el post: $post";
-});
+//* todo ruta para eliminar un registro
+Route::delete('.posts/{post}', [PostControlles::class, 'destroy']);
